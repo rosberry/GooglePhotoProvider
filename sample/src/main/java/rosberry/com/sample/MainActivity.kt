5911e0a1aa70863294b388855cd28c1e7d5e0044
enagibin <enagibin@gmail.com>
2020-04-28 14:15:31 +0600

    first touch on GoogleMediaProvider

--- sample/src/main/java/rosberry/com/sample/MainActivity.kt
+++ sample/src/main/java/rosberry/com/sample/MainActivity.kt
@@ -8,11 +8,19 @@ package rosberry.com.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rosberry.android.googlephotoprovider.CloudMediaApi
+import com.rosberry.android.googlephotoprovider.CloudMediaProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private val cloudMediaProvider by lazy {
+        CloudMediaProvider(this, , "", "")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.a_main)
+
+
     }
 }
